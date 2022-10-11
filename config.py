7ca133dae6eb024f2317157caae044f4821c49d7
saurabhrbirhade <saurabhbirhadechamp.com>
2022-10-11 11:31:55 +0530

    path added

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
+import os
+
 port_no = 5555
-model_path = r"C:\Users\saura\DATA_SCIENCE\Projects\movies_project\project_package\Movie_revenue_model.pkl"
-encoded_data_path = r"C:\Users\saura\DATA_SCIENCE\Projects\movies_project\project_package\movie_encoded_data.json"
+model_path = os.path.join('PROJECT_PACKAGE','Movie_revenue_model.pkl')
+encoded_data_path = os.path.join('PROJECT_PACKAGE','movie_encoded_data.json')
